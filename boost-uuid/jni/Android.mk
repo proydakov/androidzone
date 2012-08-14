c5302645a4c0f620b6e3f79b34a63be3d8da3913
lord.tiran <lord.tiran@gmail.com>
2012-08-14 14:12:30 +0400

    Added boost-uuid example

---
+++ boost-uuid/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := boost-uuid
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_LDLIBS    := -llog -landroid
+LOCAL_LDLIBS    += -lgnustl_static
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
---
+++ boost-uuid/src/com/example/android/boostuuid/BoostUuidActivity.java
@@ -0,0 +1,6 @@
+package com.example.android.boostuuid;
+
+import android.app.NativeActivity;
+
+public class BoostUuidActivity extends NativeActivity {
+}
